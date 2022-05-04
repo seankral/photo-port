bb18961bba1b7f793c80c579f3e601d16e1484fb
seankral <seankral9@gmail.com>
2022-05-04 09:38:45 -0400

    created contact controller and tests

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React, { useState } from 'react';
-import Nav from './components/Nav';
-import About from './components/About';
-import Gallery from './components/Gallery';
+import React, { useState } from "react";
+import Nav from "./components/Nav";
+import About from "./components/About";
+import Gallery from "./components/Gallery";
+import ContactForm from "./components/Contact";
 
 function App() {
   const [categories] = useState([
-    {name: 'commercial',description: 'Photos of grocery stores, food trucks, and other commercial projects' },
-    { name: 'portraits', description: 'Portraits of people in my life' },
-    { name: 'food', description: 'Delicious delicacies' },
-    { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
+    {
+      name: "commercial",
+      description:
+        "Photos of grocery stores, food trucks, and other commercial projects",
+    },
+    { name: "portraits", description: "Portraits of people in my life" },
+    { name: "food", description: "Delicious delicacies" },
+    {
+      name: "landscape",
+      description: "Fields, farmhouses, waterfalls, and the beauty of nature",
+    },
   ]);
 
+  const [contactSelected, setContactSelected] = useState(false);
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
 
   return (
@@ -21,10 +30,14 @@ function App() {
         currentCategory={currentCategory}
       ></Nav>
       <main>
-        <div>
-        <Gallery currentCategory={currentCategory}></Gallery>
-          <About></About>
-        </div>
+        {!contactSelected ? (
+          <>
+            <Gallery currentCategory={currentCategory}></Gallery>
+            <About></About>
+          </>
+        ) : (
+          <ContactForm></ContactForm>
+        )}
       </main>
     </div>
   );
